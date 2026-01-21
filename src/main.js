b05fd4dcc5263c45ba4dddeccfdb5a690554880d
silviolimeirawork-gif <silvio.limeira.work@gmail.com>
2026-01-21 20:19:26 -0300

    Version 1.1

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-// Importa o CSS do Bootstrap
+// Importa o CSS 4o Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 // Importe os JS do Bootstrap (Opcional, se que queser usar componentes Javascrript como modais,
 // dropdowns, etc.)
